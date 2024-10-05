1b1e57a5c252a42a244a0553f3a99c8c8c8eabc9
manikandankarmegam <manik19102000@gmail.com>
2024-10-06 04:48:34 +0530

    db to sqlite

--- uwis/settings.py
+++ uwis/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'uwis.wsgi.application'
 # #Database
 # # # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'if0_37454966_utoc_db',
-        'USER': 'if0_37454966',
-        'PASSWORD': 'sriharig',
-        'HOST': 'sql100.infinityfree.com',
-        'PORT': '3306',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / "db.sqlite3",
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'if0_37454966_utoc_db',
+#         'USER': 'if0_37454966',
+#         'PASSWORD': 'sriharig',
+#         'HOST': 'sql100.infinityfree.com',
+#         'PORT': '3306',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 
