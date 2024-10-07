db1026166555e3c5a740f8bed57e5199741c9ddf
santhosh <santhoshkasilingam2000@gmail.com>
2024-10-07 23:04:15 +0800

    static

--- uwis/settings.py
+++ uwis/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'https://darling-kleicha-045d2c.netlify.app/'
 import os
 MEDIA_URL='media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')  
