04b7cf7b9e4209a8579d914463b1a35dca2f10a0
santhosh <santhoshkasilingam2000@gmail.com>
2024-10-07 22:38:48 +0800

    new

--- uwis/settings.py
+++ uwis/settings.py
@@ -138,6 +138,8 @@ import os
 MEDIA_URL='media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')  
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS=[
     BASE_DIR/'static'
 ]
